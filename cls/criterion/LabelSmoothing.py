aea5355effb76324a1680a73f3f74fa8510a8c13
LoveLetter <48119351+LcenArthas@users.noreply.github.com>
2020-02-24 21:37:03 +0800

    Add files via upload

---
+++ cls/criterion/LabelSmoothing.py
@@ -0,0 +1,118 @@
+
+import torch
+import torch.nn as nn
+
+class CrossEntropyLabelSmooth(nn.Module):
+    """Cross entropy loss with label smoothing regularizer.
+
+    Reference:
+    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
+    Equation: y = (1 - epsilon) * y + epsilon / K.
+
+    Args:
+        num_classes (int): number of classes.
+        epsilon (float): weight.
+    """
+    def __init__(self, num_classes, epsilon=0.1, use_gpu=True):
+        super(CrossEntropyLabelSmooth, self).__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+        self.use_gpu = use_gpu
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, inputs, targets):
+        """
+        Args:
+            inputs: prediction matrix (before softmax) with shape (batch_size, num_classes)
+            targets: ground truth labels with shape (num_classes)
+        """
+        log_probs = self.logsoftmax(inputs)
+        targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1)
+        if self.use_gpu: targets = targets.cuda()
+        targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
+        loss = (- targets * log_probs).mean(0).sum()
+        return loss
+
+class LSR(nn.Module):
+
+    def __init__(self, e=0.1, reduction='mean'):
+        super().__init__()
+
+        self.log_softmax = nn.LogSoftmax(dim=1)
+        self.e = e
+        self.reduction = reduction
+    
+    def _one_hot(self, labels, classes, value=1):
+        """
+            Convert labels to one hot vectors
+        
+        Args:
+            labels: torch tensor in format [label1, label2, label3, ...]
+            classes: int, number of classes
+            value: label value in one hot vector, default to 1
+        
+        Returns:
+            return one hot format labels in shape [batchsize, classes]
+        """
+
+        one_hot = torch.zeros(labels.size(0), classes)
+
+        #labels and value_added  size must match
+        labels = labels.view(labels.size(0), -1)
+        value_added = torch.Tensor(labels.size(0), 1).fill_(value)
+
+        value_added = value_added.to(labels.device)
+        one_hot = one_hot.to(labels.device)
+
+        one_hot.scatter_add_(1, labels, value_added)
+
+        return one_hot
+
+    def _smooth_label(self, target, length, smooth_factor):
+        """convert targets to one-hot format, and smooth
+        them.
+
+        Args:
+            target: target in form with [label1, label2, label_batchsize]
+            length: length of one-hot format(number of classes)
+            smooth_factor: smooth factor for label smooth
+        
+        Returns:
+            smoothed labels in one hot format
+        """
+        one_hot = self._one_hot(target, length, value=1 - smooth_factor)
+        one_hot += smooth_factor / length
+
+        return one_hot.to(target.device)
+
+    def forward(self, x, target):
+
+        if x.size(0) != target.size(0):
+            raise ValueError('Expected input batchsize ({}) to match target batch_size({})'
+                    .format(x.size(0), target.size(0)))
+
+        if x.dim() < 2:
+            raise ValueError('Expected input tensor to have least 2 dimensions(got {})'
+                    .format(x.size(0)))
+
+        if x.dim() != 2:
+            raise ValueError('Only 2 dimension tensor are implemented, (got {})'
+                    .format(x.size()))
+
+
+        smoothed_target = self._smooth_label(target, x.size(1), self.e)
+        x = self.log_softmax(x)
+        loss = torch.sum(- x * smoothed_target, dim=1)
+
+        if self.reduction == 'none':
+            return loss
+        
+        elif self.reduction == 'sum':
+            return torch.sum(loss)
+        
+        elif self.reduction == 'mean':
+            return torch.mean(loss)
+        
+        else:
+            raise ValueError('unrecognized option, expect reduction to be one of none, mean, sum')
+
